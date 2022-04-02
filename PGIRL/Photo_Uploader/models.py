c247961b3ba7f896776c95eec1e96fa137987d3c
JacobEnochCMSC478 <jenoch1@umbc.edu>
2022-04-02 13:51:05 -0400

    Began work on a database model for photos

--- PGIRL/Photo_Uploader/models.py
+++ PGIRL/Photo_Uploader/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Photo_Data(models.model):
+    user_id = models.TextField()  # Unique user ID assigned to users when they create an account
+    actual_image = models.ImageField()
+    date_added = models.DateField()
