39e9b484b4c3858ee3d435a3193075e7a779487c
JacobEnochCMSC478 <jenoch1@umbc.edu>
2022-04-06 01:09:54 -0400

    Added default values to some fields, created and applied migrations to DB

--- PGIRL/Photo_Uploader/models.py
+++ PGIRL/Photo_Uploader/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
-class Photo_Data(models.model):
+
+class Photo_Data(models.Model):
     user_id = models.TextField()  # Unique user ID assigned to users when they create an account
-    actual_image = models.ImageField()
+    actual_image = models.ImageField(upload_to='uploads/')
     date_added = models.DateField()
+    verified_status = models.BooleanField(default=False)  # Photos will not be verified by default
+
+    # Each photo will have stats associated with them - these will be automatically rolled when added to the database
+    stat_hp = models.IntegerField(default=0)
+    stat_attack = models.IntegerField(default=0)
+    stat_defense = models.IntegerField(default=0)
+    stat_speed = models.IntegerField(default=0)
