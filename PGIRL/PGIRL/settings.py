f3fa93305d386acf9516cad1809cc9c518feb563
natolyb1 <natolyb1@umbc.edu>
2022-04-26 13:15:12 -0400

    added new verification app and adjusted settings

--- PGIRL/PGIRL/settings.py
+++ PGIRL/PGIRL/settings.py
@@ -32,6 +32,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'Inventory.apps.InventoryConfig',
     'Photo_Uploader',
+    'background_task',
+    'verification',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- PGIRL/PGIRL/urls.py
+++ PGIRL/PGIRL/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('inventory/', include('Inventory.urls')),
     path('', include('Photo_Uploader.urls')),
+    path('', include('verification.urls')),
 ]
 
 if settings.DEBUG:
