00579b103441386dcf8aacd0d35bbced8b655bd8
natolyb1 <natolyb1@umbc.edu>
2022-04-27 16:16:29 -0400

    included buttons on the html, alters pet attributes depending on user input

--- PGIRL/verification/urls.py
+++ PGIRL/verification/urls.py
@@ -2,7 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.urls import re_path as url
 from . import views
+#import Photo_Uploader.views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('/answer_yes', views.answer_yes, name='answer_yes'),
+    path('/answer_no', views.answer_no, name='answer_no'),
 ]
--- PGIRL/verification/views.py
+++ PGIRL/verification/views.py
@@ -1,16 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from Photo_Uploader.models import Photo_Data
+import random
+
+pet = NULL #drastic measures
 
 # Create your views here.
 def index(request):
-    pet = Photo_Data.objects.filter(verified_status=False)[0]
+    items = list(Photo_Data.objects.filter(verified_status=False))
+    global pet
+    pet = random.choice(items)
+
     cont = {
         "image":pet.image,
         "pet_name":pet.pet_name
     }
     return render(request, "verification/verify_pet.html", context=cont)
 
-#create popup for user?
+#if they pressed yes
+def answer_yes(request):
+    global pet
+    pet.increment_passes()
+    if pet.get_passes() >= 3:
+        #get a stat boost! :D
+        pet.stat_hp+=5
+        print("Increased health for that pet.")
+        pass
+
+    print("Increased passes for that image.")
+    return HttpResponseRedirect(reverse('index'))
+
+#if they pressed no
+def answer_no(request):
+    #L :(
+    global pet
+    pet.increment_strikes()
+    print("Increased strikes for that image.")
+    return HttpResponseRedirect(reverse('index'))
 
 #Depending on what the user presses, increment strikes or passes
