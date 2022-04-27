2ddc3ed2bb193e37ecd51989980a8ce49e24247f
natolyb1 <natolyb1@umbc.edu>
2022-04-27 16:18:55 -0400

    deleted faulty original verify

--- PGIRL/verify/verify/verify.py
+++
@@ -1,6 +0,0 @@
-"""  Goals for this file:
---Function (Django background task)
---Create pop-up window
---The window asks the user (Is this a ____?) The user chooses yes or no.
---This function will be called by Jacob's queue.
-"""
