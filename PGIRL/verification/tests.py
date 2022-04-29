efe72ad1f8b00e4f009bedabc8de4483277ed4bd
natolyb1 <natolyb1@umbc.edu>
2022-04-28 21:20:34 -0400

    Progress on tests! (only 1/5 fail hehe)

--- PGIRL/verification/tests.py
+++ PGIRL/verification/tests.py
@@ -1,16 +1,28 @@
-from django.test import TestCase
+#from django.test import TestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from Photo_Uploader.models import Photo_Data
 from datetime import datetime
 from django.test.client import Client
 from django.urls import reverse
 from PIL import Image
 from io import BytesIO
+from django.http import HttpRequest
+from verification.views import verify
+
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
 
-# Create your tests here.
-class VerifyTest(self):
+# Create your tests here. This should run the server alongside testing.. for selenium.
+class VerifyTest(StaticLiveServerTestCase):
+
+    def setUp(self):
+        #add these to the DB
+        current_time = datetime.now()
+        Photo_Data.objects.all().delete()
+        Photo_Data.objects.create(user_id=1, image='test_uploads/big_oof.PNG', date_added=current_time, strikes=2,passes=2)
+        pass
+
     #Am I sent to the error screen if nothing's in the DB?
     def test_site(self):
         c = Client()
@@ -19,7 +31,7 @@ class VerifyTest(self):
 
         # Is this the right page tho
         request = HttpRequest()
-        response2 = home_page(request)
+        response2 = verify(request)
         expected_html = render_to_string('verify_pet.html', request=request)
         self.assertEqual(response2.content.decode(), expected_html)
 
@@ -28,16 +40,9 @@ class VerifyTest(self):
 
         #empty page?
         request = HttpRequest()
-        response2 = home_page(request)
+        response2 = verify(request)
         expected_html = render_to_string('empty_error.html', request=request)
         self.assertEqual(response2.content.decode(), expected_html)
-        pass
-
-    def setUp(self):
-        #add these to the DB
-        current_time = datetime.now()
-        Photo_Data.objects.all().delete()
-        Photo_Data.objects.create(user_id=1, image='test_uploads/big_oof.PNG', date_added=current_time, strikes=2,passes=2)
         pass
 
     #backup option
@@ -52,21 +57,23 @@ class VerifyTest(self):
     def test_voting_yes(self):
         #Get the pet
         pet1 = Photo_Data.objects.get(user_id=1)
-        init_hp = pet.stat_hp
+        init_hp = pet1.stat_hp
 
-        selenium = webdriver.Chrome()
-        selenium.get('http://127.0.0.1:8000/verification/')
+        selenium = webdriver.Chrome(ChromeDriverManager().install())
+        selenium.get('%s%s' % (self.live_server_url, '/verification/'))
 
         #Test yes
         #This will throw an error if it can't find the button
         yes_btn = selenium.find_element_by_id('yesbtn')
         print("Yes button found. Continue with the test.\n")
 
+        print("Number of passes was: ", pet1.get_passes())
         #No error thrown? Let's goo! Pass :)
         print("Pressing yes.")
         yes_btn.click()
 
         #Check if the number of passes increased
+        print("Now its: ", pet1.get_passes())
         self.assertEqual(pet1.get_passes(), 3, "Passes incremented correctly")
 
         self.assertEqual(pet1.verified_status, True, "Verification flag flipped.")
@@ -86,10 +93,10 @@ class VerifyTest(self):
     # Does the pet get removed if strikes reaches the threshold?
     def test_vote_no(self):
         # Get the pet
-        pet1 = Photo_Data.objects.get(user_id=1)
+        #pet1 = Photo_Data.objects.get(user_id=1)
 
-        selenium = webdriver.Chrome()
-        selenium.get('http://127.0.0.1:8000/verification/')
+        selenium = webdriver.Chrome(ChromeDriverManager().install())
+        selenium.get('%s%s' % (self.live_server_url, '/verification/'))
 
         # This will throw an error if it can't find the button
         no_btn = selenium.find_element_by_id('nobtn')
@@ -114,7 +121,7 @@ class VerifyTest(self):
 
         # The page should be on default error page (no more pets to validate!)
         request = HttpRequest()
-        response2 = home_page(request)
+        response2 = verify(request)
         expected_html = render_to_string('empty_error.html', request=request)
         self.assertEqual(response2.content.decode(), expected_html)
 
